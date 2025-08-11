a4dd27da8415599da70cd91b7e853b893f95011f
devgreact <dev.greact@gmail.com>
2025-08-11 11:00:04 +0900

    [docs] todo 앱 제작하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
 import { useState } from 'react';
 import TodoList from './components/todos/TodoList';
 import TodoWrite from './components/todos/TodoWrite';
+
+// 공통으로 사용하는 type 정의 및 interface 는 별도의 폴더에 보관하자.
 import { ITodoType, TodoType } from './types/todoType';
+
+// 테스트를 위한 목업 데이터 (/src/api/dummy.ts 추천)
+const initialTodos: TodoType[] = [
+  { id: 'a', title: '제목 1 입니다', completed: false },
+  { id: 'b', title: '제목 2 입니다', completed: true },
+  { id: 'c', title: '제목 3 입니다', completed: false },
+  { id: 'd', title: '제목 4 입니다', completed: true },
+  { id: 'e', title: '제목 5 입니다', completed: false },
+];
 
 function App(): JSX.Element {
   // ts 자리
   // {id: "", title: "", completed:false}
-  const [todos, setTodos] = useState<(ITodoType | TodoType)[]>([]);
+  const [todos, setTodos] = useState<(ITodoType | TodoType)[]>(initialTodos);
 
   // todos 를 업데이트 하는 함수
   const handleTodoUpdate = (): void => {
     // setTodos(???)
   };
   // todo 목록에서 실행할 함수들
-  const onToggle = (): void => {};
-  const onDelte = (): void => {};
-  const onEdit = (): void => {};
+  const onToggle = (id: string): void => {
+    console.log('onToggle : ', id);
+    // 전달 받은 ID 를 이용해서 map 으로 찾아서 id 가 같으면 completed 변경
+    const arr: TodoType[] = todos.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+    );
+    setTodos(arr);
+  };
+  const onDelte = (id: string): void => {
+    console.log('onDelte : ', id);
+    // 전달 받은 ID 를 제외한 나머지 만 모아서 목록 변경
+    const arr: TodoType[] = todos.filter(todo => todo.id !== id);
+    setTodos(arr);
+  };
+
+  const onEdit = (): void => {
+    console.log('onEdit');
+  };
   // tsx 자리
   return (
     <div>
--- src/components/todos/TodoItem.tsx
+++ src/components/todos/TodoItem.tsx
@@ -1,11 +1,38 @@
+import { TodoType } from '@/types/todoType';
+import React from 'react';
+
 type TodoItemProps = {
-  onToggle: () => void;
-  onDelte: () => void;
+  todo: TodoType;
+  onToggle: (id: string) => void;
+  onDelte: (id: string) => void;
   onEdit: () => void;
 };
 
-const TodoItem = ({ onToggle, onDelte, onEdit }: TodoItemProps) => {
-  return <div>TodoItem</div>;
+const TodoItem = ({ todo, onToggle, onDelte, onEdit }: TodoItemProps) => {
+  // 수정은 별도의 입력창 구성으로 수정 후 값 만 업데이트
+  const handleEdit = () => {
+    console.log('여기에서 내용을 수정하는 기능 작성 후 완료된 데이터 전송');
+    onEdit();
+  };
+
+  // CSS 객체 만들기
+  const liStyle: React.CSSProperties = {
+    display: 'flex',
+    gap: '10px',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: todo.completed ? 'gray' : 'red',
+    textDecoration: todo.completed ? 'line-through' : 'none',
+  };
+
+  return (
+    <li style={liStyle}>
+      <input type="checkbox" onChange={() => onToggle(todo.id)} checked={todo.completed} />
+      <span>{todo.title}</span>
+      <button onClick={handleEdit}>수정</button>
+      <button onClick={() => onDelte(todo.id)}>삭제</button>
+    </li>
+  );
 };
 
 export default TodoItem;
